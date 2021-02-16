c79544a52b12080ad78a356ff24c76e037c47f31
veltus5184 <nathakorn.jumderm@gmail.com>
2021-02-16 13:10:21 +0700

    Create work4.py

---
+++ work4.py
@@ -0,0 +1,10 @@
+print("Coded by เด็กชายนฐกร จำเดิม")
+print("Circle Calculation")
+radius = 18/2
+pi = 22/7
+print("Radius: " + str(radius))
+
+circleArea = 22/7 * radius ** 2
+print("พื้นที่วงกลม =", circleArea)
+circleLength = 2 * 22/7 * radius
+print("เส้นรอบวง =", circleLength)
