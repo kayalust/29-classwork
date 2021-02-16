0188188ad152e5d31569ddfbb7235cf59fc5f17d
veltus5184 <nathakorn.jumderm@gmail.com>
2021-02-16 12:53:05 +0700

    Create work1.py

---
+++ work1.py
@@ -0,0 +1,5 @@
+print("Please input a number for the length.")
+length =  int(input())
+print("Please input a number for the width")
+width = int(input())
+print("The rectangles size is " + str(length * width))
