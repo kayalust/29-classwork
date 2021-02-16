b6a4d4fe1317f13b14c802ae3dbb788941ac81b4
veltus5184 <nathakorn.jumderm@gmail.com>
2021-02-16 13:09:51 +0700

    Create work3.py

---
+++ work3.py
@@ -0,0 +1,8 @@
+print("โปรแกรมหาทุกอย่างของวงกลม")
+pi = 22/7
+radius = input() * 2
+area1 = float(pi) * int(radius)
+print("พื้นที่วงกลม = " + str(float(area1)))
+print("Circle Line Length")
+area2 = 2 * float(pi) * int(radius)
+print("ความยาวรอบวง = " + str(area2))
