1d24763209725ba3de2693ce6ee8db563d61f200
veltus5184 <nathakorn.jumderm@gmail.com>
2021-02-16 13:12:02 +0700

    Create work9.py

---
+++ work9.py
@@ -0,0 +1,12 @@
+weight = float(input("นํ้าหนัก: "))
+
+if (weight <= 40) :
+  print("ขาดสารอาหาร")
+elif (weight <= 50) :
+  print("นํ้าหนักพอดี")
+elif (weight <= 65) :
+  print("สารอาหารมากหน่อย")
+elif (weight <= 80) :
+  print("สารอาหารเยอะ")
+else :
+  print("อ้วนมาาาาาาาาาาาาาาก")
