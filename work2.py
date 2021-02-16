7f689925ae30f654f5138971370c1a4f8733d2d4
veltus5184 <nathakorn.jumderm@gmail.com>
2021-02-16 13:09:33 +0700

    Create work2.py

---
+++ work2.py
@@ -0,0 +1,8 @@
+print("Coded by เด็กชายนฐกร จำเดิม")
+print("คำนวณพื้นที่สามเหลี่ยม")
+base = 10
+print("ความยาวฐาน =", base)
+height = 20
+print("ความสูง =", height)
+area = 1/2 * 10 * 20
+print("พื้นที่สามเหลี่ยม: " + str(float(area)))
